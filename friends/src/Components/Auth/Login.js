f70a90f4111c38a801f976fd9e7f74373f603785
fjhansen <58237856+fjhansen@users.noreply.github.com>
2020-03-24 12:45:30 -0700

    login form

--- friends/src/Components/Auth/Login.js
+++ friends/src/Components/Auth/Login.js
@@ -1,11 +1,52 @@
 import React, { useState } from 'react';
-
+import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react'
 
 export default function Login(props) {
-  const[state, setState] = useState([]);
+  const[data, setData] = useState({
+    email: "",
+    password:"",
+  });
 
-
+  const handleChange = (event) => {
+    setData({
+      ...data,
+      [event.target.name]: event.target.value,
+    })
+  }
   return(
-    <div>Login</div>
+    <Grid textAlign="center" vertialAlign="middle">
+      <Grid.Column style={{maxWidth: 450}}>
+       <Header>Login</Header> 
+       <Form>
+         <Segment stacked>
+           <Form.Input
+              fluid name="email" 
+              icon="mail" 
+              iconPosition="left" 
+              placeholder="Email" 
+              type="email"
+              value={data.email}
+              onChange={handleChange}
+           />
+
+           <Form.Input
+              fluid name="password" 
+              icon="key" 
+              iconPosition="left" 
+              placeholder="Password" 
+              type="password"
+              value={data.password}
+              onChange={handleChange}
+           />
+
+           <Button type="submit">Sign In</Button>
+         </Segment>
+         
+       </Form>
+      </Grid.Column>
+      
+
+
+    </Grid>
   )
 }
